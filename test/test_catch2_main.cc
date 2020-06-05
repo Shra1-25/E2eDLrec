b18751eaa5585b6f0dee75771bf4eb2a92c22b7a
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-06 02:01:35 +0530

    Update test_catch2_main.cc

--- test/test_catch2_main.cc
+++ test/test_catch2_main.cc
@@ -4,8 +4,8 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include "tensorflow/core/public/session.h"
-#include "tensorflow/core/graph/default_device.h"
+//#include "tensorflow/core/public/session.h"
+//#include "tensorflow/core/graph/default_device.h"
 using namespace tensorflow;
 using namespace std;
 
