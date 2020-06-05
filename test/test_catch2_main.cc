565f849d4ad179416d27e42bd6d721112e463f41
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-06 01:56:36 +0530

    Update test_catch2_main.cc

--- test/test_catch2_main.cc
+++ test/test_catch2_main.cc
@@ -1,2 +1,15 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "tensorflow/core/public/session.h"
+#include "tensorflow/core/graph/default_device.h"
+using namespace tensorflow;
+using namespace std;
+
+int main()
+{
+ std::cout<<"Hello"<<endl; 
+}
