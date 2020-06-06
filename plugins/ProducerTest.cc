b81bbef3cd7860eff02251f61f275126dee2111d
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-07 01:56:06 +0530

    Update ProducerTest.cc

--- plugins/ProducerTest.cc
+++ plugins/ProducerTest.cc
@@ -61,7 +61,7 @@ class ProducerTest : public edm::stream::EDProducer<> {
       //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
 
       // ----------member data ---------------------------
-      std::string graph_definition="graph3.pb"; 
+      //std::string graph_definition="graph3.pb"; 
 };
 
 //
@@ -127,38 +127,46 @@ ProducerTest::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
    iSetup.get<SetupRecord>().get(pSetup);
 */
  tensorflow::Session* session;
- tensorflow::GraphDef graph_def;
+ //tensorflow::GraphDef graph_def;
  tensorflow::SessionOptions opts;
- std::vector<Tensor> outputs; // Store outputs
+ std::vector<tensorflow::Tensor> outputs; // Store outputs
  
- tensorflow::Tensor x(DT_FLOAT, TensorShape({100, 32}));
- tensorflow::Tensor y(DT_FLOAT, TensorShape({100, 8}));
+ tensorflow::Tensor x(tensorflow::DT_FLOAT, tensorflow::TensorShape({100, 32}));
+ tensorflow::Tensor y(tensorflow::DT_FLOAT, tensorflow::TensorShape({100, 8}));
  auto _XTensor = x.matrix<float>();
  auto _YTensor = y.matrix<float>();
  _XTensor.setRandom();
  _YTensor.setRandom();
  
- TF_CHECK_OK(ReadBinaryProto(Env::Default(), graph_definition, &graph_def));
+ //TF_CHECK_OK(ReadBinaryProto(tensorflow::Env::Default(), graph_definition, &graph_def));
+ // load the graph definition, i.e. an object that contains the computational graph
+ tensorflow::GraphDef* graphDef = tensorflow::loadGraphDef("graph3.pb");
  // Set GPU options
  //graph::SetDefaultDevice("/gpu:0", &graph_def);
  //opts.config.mutable_gpu_options()->set_per_process_gpu_memory_fraction(0.5);
  //opts.config.mutable_gpu_options()->set_allow_growth(true);
  
  // create a new session
- TF_CHECK_OK(NewSession(opts, &session));
+ //TF_CHECK_OK(NewSession(opts, &session));
  
  // Load graph into session
- TF_CHECK_OK(session->Create(graph_def));
+ //TF_CHECK_OK(session->Create(graph_def));
+ 
+ // create a session
+ tensorflow::Session* session = tensorflow::createSession(graphDef);
  
  // Initialize our variables
- TF_CHECK_OK(session->Run({}, {}, {"init_all_vars_op"}, nullptr));
+ //TF_CHECK_OK(session->Run({}, {}, {"init_all_vars_op"}, nullptr));
+ tensorflow::run(session, {}, {}, {"init_all_vars_op"}, nullptr);
  
  for (int i = 0; i < 10; ++i) {
         
-  TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {"cost"}, {}, &outputs)); // Get cost
+  //TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {"cost"}, {}, &outputs)); // Get cost
+  tensorflow::run(session, { { "x", x }, {"y", y} }, { "cost" }, {}, &outputs);
   float cost = outputs[0].scalar<float>()(0);
   std::cout << "Cost: " <<  cost << std::endl;
-  TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {}, {"train"}, nullptr)); // Train
+  //TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {}, {"train"}, nullptr)); // Train
+  tensorflow::run(session, { { "x", x }, {"y", y} }, {}, {"train"}, &outputs);
   outputs.clear();
   
   session->Close();
