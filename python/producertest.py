f86397525179a77b1b2f0326910bd4ca68919c5e
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-07 02:48:06 +0530

    Update producertest.py

--- python/producertest.py
+++ python/producertest.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("OWNPARTICLES")
+process = cms.Process("EXAMPLE_1")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
@@ -8,20 +8,20 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-       'file:/afs/cern.ch/cms/Tutorials/TWIKI_DATA/CMSDataAnaSch_RelValZMM536.root'
+    #fileNames = cms.untracked.vstring(
+    #   'file:/afs/cern.ch/cms/Tutorials/TWIKI_DATA/CMSDataAnaSch_RelValZMM536.root'
     )
 )
 
 #from ProdTutorial.TrackAndPointsProducer.trackandpointsproducer_cfi import *
-process.MuonTrackPoints = cms.EDProducer('TrackAndPointsProducer'
-        ,src    =cms.InputTag('globalMuons')
+process.test1 = cms.EDProducer('ProducerTest'
+        #,src    =cms.InputTag('globalMuons')
 
 )
 
-process.TrackTrackPoints = cms.EDProducer('TrackAndPointsProducer'
-        ,src    =cms.InputTag('generalTracks')
-)
+#process.TrackTrackPoints = cms.EDProducer('TrackAndPointsProducer'
+#        ,src    =cms.InputTag('generalTracks')
+#)
 
 process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('myOutputFile.root')
@@ -34,6 +34,6 @@ process.out = cms.OutputModule("PoolOutputModule",
 )
 
 
-process.p = cms.Path(process.MuonTrackPoints*process.TrackTrackPoints)
+process.p = cms.Path(process.test1)
 
 process.e = cms.EndPath(process.out)
