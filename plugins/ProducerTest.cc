4c75f105601d24ca3e6c8ccba97f58193de0a586
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-07 02:15:30 +0530

    Update ProducerTest.cc

--- plugins/ProducerTest.cc
+++ plugins/ProducerTest.cc
@@ -35,7 +35,7 @@
 #include "FWCore/Utilities/interface/StreamID.h"
 #include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
 
-//using namespace tensorflow;
+using namespace tensorflow;
 using namespace std;
 
 
@@ -174,6 +174,9 @@ ProducerTest::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
  //std::cout<<_YTensor(0,0)<<" "<<_YTensor(0,1)<<" "<<_YTensor(0,2)<<" "<<_YTensor(0,3)<<" "<<_YTensor(0,4)<<" "<<_YTensor(0,5)<<" "<<_YTensor(0,6)<<" "<<_YTensor(0,7)<<" "<<_YTensor(0,8)<<" "<<_YTensor(0,9)<<endl;
  //std::cout<<_YTensor(1,0)<<" "<<_YTensor(1,1)<<" "<<_YTensor(1,2)<<" "<<_YTensor(1,3)<<" "<<_YTensor(1,4)<<" "<<_YTensor(1,5)<<" "<<_YTensor(1,6)<<" "<<_YTensor(1,7)<<" "<<_YTensor(1,8)<<" "<<_YTensor(1,9)<<endl;
  std::cout<<"All done"<<endl;
+ // cleanup
+ //tensorflow::closeSession(session);
+ //delete graphDef;
 }
 
 // ------------ method called once each stream before processing any runs, lumis or events  ------------
