592fb3a77d8c089f04b7d23cdbcfa1d688e69334
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-09 10:22:46 +0530

    Update test_catch2_ProducerTest.cc

--- test/test_catch2_ProducerTest.cc
+++ test/test_catch2_ProducerTest.cc
@@ -1,5 +1,5 @@
 #include "catch.hpp"
-#include "/home/cmsusr/CMSSW_10_6_8/src/demo/plugins/ProducerTest.h"
+#include "\\home\\cmsusr\\CMSSW_10_6_8\\src\\demo\\plugins\\ProducerTest.h"
 #include "FWCore/TestProcessor/interface/TestProcessor.h"
 #include "FWCore/Utilities/interface/Exception.h"
 
