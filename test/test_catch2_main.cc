3b7efdb3999168f49b27c9c87e749d2bb9a8308b
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-09 09:55:38 +0530

    Update test_catch2_main.cc

--- test/test_catch2_main.cc
+++ test/test_catch2_main.cc
@@ -1,25 +1,2 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-/*#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <stdexcept>
-#include <cppunit/extensions/HelperMacros.h>
-#include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
-#include "testBase.h"
-//#include "tensorflow/core/public/session.h"
-//#include "tensorflow/core/graph/default_device.h"
-//using namespace tensorflow;
-using namespace std;
-
-class testGraphLoading : public test_graphs {
-  CPPUNIT_TEST_SUITE(testGraphLoading);
-  CPPUNIT_TEST(checkAll);
-  CPPUNIT_TEST_SUITE_END();
-
-public:
-  std::string pyScript() const override;
-  void checkAll() override;
-};
-*/
