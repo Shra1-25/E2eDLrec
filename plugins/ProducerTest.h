42cc64b54e59fb8532c8891d59cb18d79dda463a
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-09 10:19:18 +0530

    Update ProducerTest.h

--- plugins/ProducerTest.h
+++ plugins/ProducerTest.h
@@ -22,7 +22,7 @@ class ProducerTest : public edm::stream::EDProducer<> {
 
    private:
       virtual void beginStream(edm::StreamID) override;
-      virtual void produce(edm::Event&, const edm::EventSetup&) override;
+      //virtual void produce(edm::Event&, const edm::EventSetup&) override;
       virtual void endStream() override;
 
       //virtual void beginRun(edm::Run const&, edm::EventSetup const&) override;
