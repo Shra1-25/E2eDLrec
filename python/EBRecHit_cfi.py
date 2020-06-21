729025aa17f309d8b3428a035c4e3a3a23009a00
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-21 09:54:47 +0530

    Create EBRecHit_cfi.py

---
+++ python/EBRecHit_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms 
+
+fevt_tf = cms.EDProducer('EBRecHitProducer'
+    , reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
+    , mode = cms.string("JetLevel")
+    # Jet level cfg
+    , nJets = cms.int32(-1)
+    , minJetPt = cms.double(35.)
+    , maxJetEta = cms.double(2.4)
+    , z0PVCut  = cms.double(0.1)
+    )
