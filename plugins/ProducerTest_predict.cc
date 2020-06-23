8a951cda618a89c2e4eea9ed44f95021892ed4a2
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-23 14:57:09 +0530

    Update ProducerTest_predict.cc

--- plugins/ProducerTest_predict.cc
+++ plugins/ProducerTest_predict.cc
@@ -10,6 +10,7 @@ void ProducerTest::predict_tf(){
  tensorflow::SessionOptions opts;
  std::vector<tensorflow::Tensor> outputs; // Store outputs
  std::string graph_definition="\\home\\cmsusr\\CMSSW_10_6_8\\src\\ProdTutorial\\ProducerTest\\plugins\\graph3.pb";
+ std::cout<<"Done1"<<endl;
  
  tensorflow::Tensor x(tensorflow::DT_FLOAT, tensorflow::TensorShape({100, 32}));
  tensorflow::Tensor y(tensorflow::DT_FLOAT, tensorflow::TensorShape({100, 8}));
@@ -17,10 +18,12 @@ void ProducerTest::predict_tf(){
  auto _YTensor = y.matrix<float>();
  _XTensor.setRandom();
  _YTensor.setRandom();
+ std::cout<<"Done2"<<endl;
  
  //TF_CHECK_OK(ReadBinaryProto(Env::Default(), graph_definition, &graph_def));
  // load the graph definition, i.e. an object that contains the computational graph
  tensorflow::GraphDef* graphDef = tensorflow::loadGraphDef("ProducerTest/plugins/graph3.pb");
+ std::cout<<"Done3"<<endl;
  // Set GPU options
  //graph::SetDefaultDevice("/gpu:0", &graph_def);
  //opts.config.mutable_gpu_options()->set_per_process_gpu_memory_fraction(0.5);
@@ -34,7 +37,7 @@ void ProducerTest::predict_tf(){
  
  // create a session
  session = tensorflow::createSession(graphDef);
- 
+ std::cout<<"Done4"<<endl;
  // Initialize our variables
  //TF_CHECK_OK(session->Run({}, {}, {"init_all_vars_op"}, nullptr));
  //tensorflow::run(session, {}, {"init_all_vars_op"}, nullptr);
@@ -43,6 +46,7 @@ void ProducerTest::predict_tf(){
         
  //TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {"cost"}, {}, &outputs)); // Get cost
  tensorflow::run(session, { { "x", x }, {"y", y} }, { "cost" }, &outputs);
+ std::cout<<"Done5"<<endl;
  float cost = outputs[0].scalar<float>()(0);
  std::cout << "Cost: " <<  cost << std::endl;
  //TF_CHECK_OK(session->Run({{"x", x}, {"y", y}}, {}, {"train"}, nullptr)); // Train
