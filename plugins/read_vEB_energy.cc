11d2c8a842373b197aec6305940afb491f1de2d0
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-25 10:40:06 +0530

    Create read_vEB_energy.cc

---
+++ plugins/read_vEB_energy.cc
@@ -0,0 +1,24 @@
+#include "ProdTutorial/ProducerTest/plugins/ProducerTest.h"
+#include <iostream>
+using namespace std;
+
+std::vector<float>& read_vEB_energy(int vec_size)
+ {
+ 
+    TFile *fr = TFile::Open("ECAL_Rechit.root","READ");
+    if (!fr) { return; }
+ 
+    std::vector<float> *vEB_energy_read = 0;
+    TTree *tr = (TTree*)f1->Get("vec_tree");
+    tr->SetBranchAddress("vEB_energy_vec",&vEB_energy_read);
+    for (int i=0;i<vec_size;i++){
+      tr->GetEntry(i);
+    } 
+    for (int j=0;j<10;j++){
+     std::cout<<vEB_energy_read->at(j)<<" ";
+    }
+    std::cout<<endl;
+    tr->ResetBranchAddresses();
+    fr->Close();
+    return *vEB_energy_read;
+ }
