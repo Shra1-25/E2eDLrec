ea7ff3ca3d35e3855dc7722a6a3a0cdf5c6ccb98
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-27 19:46:01 +0530

    Update EBRecHit_cfi.py

--- python/EBRecHit_cfi.py
+++ python/EBRecHit_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 fevt_tf = cms.EDProducer('ProducerTest'
     , reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
+    , photonCollection = cms.InputTag('slimmedPhotons')
     , mode = cms.string("JetLevel")
     # Jet level cfg
     , nJets = cms.int32(-1)
