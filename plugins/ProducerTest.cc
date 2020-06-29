5d97b2c523b02105a1dfdd59d1282060514a513d
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-06-30 03:52:52 +0530

    Update ProducerTest.cc

--- plugins/ProducerTest.cc
+++ plugins/ProducerTest.cc
@@ -161,7 +161,7 @@ ProducerTest::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     end_x=ieta_Emax+vEB_frame_height/2;
    }
    for (int x_idx = start_x; x_idx<=end_x;x_idx++){
-    for (int y_idx = start_y; y_idx<end_y;y_idx++){
+    for (int y_idx = start_y; y_idx<=end_y;y_idx++){
      vEB_frame[x_idx-start_x][y_idx-start_y]=vEB_energy_[x_idx*vEB_energy_height+y_idx];
      std::cout<<"("<<x_idx-start_x<<","<<y_idx-start_y<<"): "<<vEB_frame[x_idx-start_x][y_idx-start_y]<<" "<<vEB_energy_[x_idx*vEB_energy_height+y_idx]<<" ";
     }
