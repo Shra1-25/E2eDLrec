c7a46b218fa45ac0f202002b941205d4cd130d51
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-10 01:44:35 +0530

    Update ProducerTest_Photon.cc

--- plugins/ProducerTest_Photon.cc
+++ plugins/ProducerTest_Photon.cc
@@ -142,7 +142,14 @@ void ProducerTest::get_photons ( const edm::Event& iEvent, const edm::EventSetup
   }*/
   std::cout<<" >> Size of frame is:"<<"("<<vEB_frame.size()<<", "<<vEB_frame[0].size()<<")"<<endl;
   std::cout<<" >> E_max at ("<<ieta_Emax<<", "<<iphi_Emax<<")is: "<<vEB_energy_[ieta_Emax*vEB_energy_width+iphi_Emax]<<endl;
-  predict_tf();
- }
+  for (auto& i : vEB_frame){
+    std::cout<<i<<" ";}
+  std::cout<<std::endl;
+  tw->Branch("vEB_frame",&vEB_frame);
+  tw->Fill();
+  fw->Write();
+  //fw->Close();
+  //predict_tf();
+  }
  return;
 }
