23c194e2dd2ae28a31f27f00424e929daad8d70e
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-11 14:02:15 +0530

    Update ProducerTest_Photon.cc

--- plugins/ProducerTest_Photon.cc
+++ plugins/ProducerTest_Photon.cc
@@ -150,7 +150,7 @@ void ProducerTest::get_photons ( const edm::Event& iEvent, const edm::EventSetup
   const char* branchname=str.c_str();
   std::cout<<" >> Number of Photons read is:"<<vEB_photon_frames.size()<<std::endl;
   std::cout<<" >> Current Photon being read is: "<<iP<<std::endl;
-  for (int i : vEB_photon_frames[vEB_photon_frames.size()-1]){
+  for (std::vector<std::vector<float>> i : vEB_photon_frames[vEB_photon_frames.size()-1]){
     for (int j=0;j<32;j++){
       for (int k=0;k<32;k++){ 
         std::cout<<i[j][k]<<" ";
