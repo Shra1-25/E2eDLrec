fb3b595af84651f87c22d0fc403e1185019a0776
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-12 02:23:22 +0530

    Update ProducerTest.cc

--- plugins/ProducerTest.cc
+++ plugins/ProducerTest.cc
@@ -127,7 +127,7 @@ ProducerTest::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
      //h_sel->Fill( 0. );;
      //return;
    }*/
-   std::auto_ptr<int> photon_classes(new std::vector<int>);
+   std::auto_ptr<std::vector<int>> photon_classes(new std::vector<int>);
   
    fillEB( iEvent, iSetup );
    /*for (unsigned int i=0;i<vEB_energy_.size();i++){
