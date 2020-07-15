5185368630b11ee880972f6bb7706f93e1fe7700
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-16 04:34:54 +0530

    Update producertest_cfg.py

--- python/producertest_cfg.py
+++ python/producertest_cfg.py
@@ -30,7 +30,8 @@ process.source = cms.Source("PoolSource",
     )
 print (" >> Loaded",len(options.inputFiles),"input files from list.")
 
-process.load("ProdTutorial.ProducerTest.EBRecHit_cfi")
+#process.load("ProdTutorial.ProducerTest.EBRecHit_cfi")
+process.load("ProdTutorial.ProducerTest.FrameInference_cfi")
 #process.fevt_tf.mode = cms.string('JetLevel')#options.processMode
 
 #process.out = cms.OutputModule("PoolOutputModule",
@@ -42,12 +43,13 @@ process.load("ProdTutorial.ProducerTest.EBRecHit_cfi")
 #      "keep *_TrackTrackPoints_*_*")
 #)
 
-process.out = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('myOutputFile.root'))
+#process.out = cms.OutputModule("PoolOutputModule",
+#    fileName = cms.untracked.string('myOutputFile.root'))
 #print " >> Processing as:",(process.fevt_tf.mode)
-process.TFileService = cms.Service("TFileService",
-    fileName = cms.string("myoutput.root")#options.outputFile
-    )
+#process.TFileService = cms.Service("TFileService",
+#    fileName = cms.string("myoutput.root")#options.outputFile
+#   )
 
-process.p = cms.Path(process.ProducerFrames)
-process.ep=cms.EndPath(process.out)
+#process.p = cms.Path(process.ProducerFrames)
+process.p = cms.Path(process.FrameInference)
+#process.ep=cms.EndPath(process.out)
