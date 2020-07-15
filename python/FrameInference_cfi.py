49b84cb5795079dbe9107a897dbe81ac95828073
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-16 04:32:33 +0530

    Create FrameInference_cfi.py

---
+++ python/FrameInference_cfi.py
@@ -0,0 +1,12 @@
+import FWCore.ParameterSet.Config as cms 
+
+FrameInference = cms.EDProducer('ProducerClassifier'
+    , photonCollection = cms.InputTag('slimmedPhotons')
+    , EBEnergy = cms.InputTag('ProducerFrames','EBEnergy')
+    #, mode = cms.string("JetLevel")
+    # Jet level cfg
+    #, nJets = cms.int32(-1)
+    #, minJetPt = cms.double(35.)
+    #, maxJetEta = cms.double(2.4)
+    #, z0PVCut  = cms.double(0.1)
+    )
