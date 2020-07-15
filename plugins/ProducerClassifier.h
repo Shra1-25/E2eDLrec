03c971e1a9c5881dcad42f7efd4f0b3dc51064b7
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-16 04:57:44 +0530

    Update ProducerClassifier.h

--- plugins/ProducerClassifier.h
+++ plugins/ProducerClassifier.h
@@ -57,10 +57,6 @@ using namespace std;
 using pat::PhotonCollection;
 using pat::PhotonRef;
 
-static const int vEB_energy_height=170;
-static const int vEB_energy_width=360;
-static const int vEB_frame_height=32;
-static const int vEB_frame_width=32;
 
 class ProducerClassifier : public edm::stream::EDProducer<> {
    public:
