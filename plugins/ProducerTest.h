0baa934d94e09eedeeb8f5b0f2cf08b0eb50b04c
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-23 16:44:47 +0530

    Update ProducerTest.h

--- plugins/ProducerTest.h
+++ plugins/ProducerTest.h
@@ -91,6 +91,8 @@ class ProducerTest : public edm::stream::EDProducer<> {
       //TProfile2D *hEB_frame;
       std::vector<float> vEB_energy_;
       std::vector<float> vEB_time_;
+      std::vector<float> vHBHE_energy_EB_;
+      std::vector<float> vHBHE_energy_;
       //std::vector<std::vector<float>> vEB_frame = std::vector<std::vector<float>> (vEB_frame_height,std::vector<float> (vEB_frame_width, 0.0));
       //float EB_frame[vEB_frame_height][vEB_frame_width];
       //std::vector<float> vEB_flat_frame = std::vector<float> (vEB_frame_height*vEB_frame_width,0.0);
