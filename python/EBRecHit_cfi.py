b00746be6469ad5eb89d7ac3826a394122fe6101
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-24 00:45:51 +0530

    Update EBRecHit_cfi.py

--- python/EBRecHit_cfi.py
+++ python/EBRecHit_cfi.py
@@ -5,6 +5,8 @@ ProducerFrames = cms.EDProducer('ProducerTest'
     , photonCollection = cms.InputTag('slimmedPhotons')
     , reducedHBHERecHitCollection = cms.InputTag('reducedHcalRecHits:hbhereco')
     , reducedEERecHitCollection = cms.InputTag('reducedEcalRecHitsEE')
+    , trackCollection = cms.InputTag("generalTracks")
+    , vertexCollection = cms.InputTag("offlinePrimaryVertices")
     #, mode = cms.string("JetLevel")
     # Jet level cfg
     #, nJets = cms.int32(-1)
