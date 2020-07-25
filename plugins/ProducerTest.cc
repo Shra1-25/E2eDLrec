873c5f9642be3e1fc791459d51f0ad6abf94edda
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-25 16:27:20 +0530

    Update ProducerTest.cc

--- plugins/ProducerTest.cc
+++ plugins/ProducerTest.cc
@@ -327,5 +327,165 @@ ProducerTest::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   descriptions.addDefault(desc);
 }
 
+const reco::PFCandidate*
+RecHitAnalyzer::getPFCand(edm::Handle<PFCollection> pfCands, float eta, float phi, float& minDr, bool debug ) {
+
+  minDr = 10;
+  const reco::PFCandidate* minDRCand = nullptr;
+  
+  for ( PFCollection::const_iterator iPFC = pfCands->begin();
+        iPFC != pfCands->end(); ++iPFC ) {
+
+    const reco::Track* thisTrk = iPFC->bestTrack();
+    if ( !thisTrk ) continue;
+
+    float thisdR = reco::deltaR( eta, phi, thisTrk->eta(), thisTrk->phi() );
+    if (debug) std::cout << "\tthisdR: " << thisdR << " " << thisTrk->pt() << " " << iPFC->particleId() << std::endl;
+
+    const reco::PFCandidate& thisPFCand = (*iPFC);
+      
+    if ( (thisdR < 0.01) && (thisdR <minDr) ) {
+      minDr    = thisdR; 
+      minDRCand = &thisPFCand;
+    }
+  }
+
+  return minDRCand;  
+}
+
+const reco::Track*
+RecHitAnalyzer::getTrackCand(edm::Handle<reco::TrackCollection> trackCands, float eta, float phi, float& minDr, bool debug ) {
+
+  minDr = 10;
+  const reco::Track* minDRCand = nullptr;
+  reco::Track::TrackQuality tkQt_ = reco::Track::qualityByName("highPurity");
+
+  for ( reco::TrackCollection::const_iterator iTk = trackCands->begin();
+        iTk != trackCands->end(); ++iTk ) {
+    if ( !(iTk->quality(tkQt_)) ) continue;  
+
+    float thisdR = reco::deltaR( eta, phi, iTk->eta(),iTk->phi() );
+    if (debug) std::cout << "\tthisdR: " << thisdR << " " << iTk->pt() << std::endl;
+
+    const reco::Track& thisTrackCand = (*iTk);
+      
+    if ( (thisdR < 0.01) && (thisdR <minDr) ) {
+      minDr    = thisdR; 
+      minDRCand = &thisTrackCand;
+    }
+  }
+
+  return minDRCand;  
+}
+
+
+
+
+int RecHitAnalyzer::getTruthLabel(const reco::PFJetRef& recJet, edm::Handle<reco::GenParticleCollection> genParticles, float dRMatch , bool debug ){
+  if ( debug ) {
+    std::cout << " Mathcing reco jetPt:" << recJet->pt() << " jetEta:" << recJet->eta() << " jetPhi:" << recJet->phi() << std::endl;
+  }
+
+  for (reco::GenParticleCollection::const_iterator iGen = genParticles->begin();
+       iGen != genParticles->end();
+       ++iGen) {
+
+    // From: (page 7/ Table 1.5.2)
+    //https://indico.desy.de/indico/event/7142/session/9/contribution/31/material/slides/6.pdf
+    //code range explanation:
+    // 11 - 19 beam particles
+    // 21 - 29 particles of the hardest subprocess
+    // 31 - 39 particles of subsequent subprocesses in multiparton interactions
+    // 41 - 49 particles produced by initial-state-showers
+    // 51 - 59 particles produced by final-state-showers
+    // 61 - 69 particles produced by beam-remnant treatment
+    // 71 - 79 partons in preparation of hadronization process
+    // 81 - 89 primary hadrons produced by hadronization process
+    // 91 - 99 particles produced in decay process, or by Bose-Einstein effects
+
+    // Do not want to match to the final particles in the shower
+    if ( iGen->status() > 99 ) continue;
+    
+    // Only want to match to partons/leptons/bosons
+    if ( iGen->pdgId() > 25 ) continue;
+
+    float dR = reco::deltaR( recJet->eta(),recJet->phi(), iGen->eta(),iGen->phi() );
+
+    if ( debug ) std::cout << " \t >> dR " << dR << " id:" << iGen->pdgId() << " status:" << iGen->status() << " nDaught:" << iGen->numberOfDaughters() << " pt:"<< iGen->pt() << " eta:" <<iGen->eta() << " phi:" <<iGen->phi() << " nMoms:" <<iGen->numberOfMothers()<< std::endl;
+
+    if ( dR > dRMatch ) continue; 
+    if ( debug ) std::cout << " Matched pdgID " << iGen->pdgId() << std::endl;
+
+    return iGen->pdgId();
+
+  } // gen particles 
+
+
+
+
+
+  return -99;
+}
+
+
+float RecHitAnalyzer::getBTaggingValue(const reco::PFJetRef& recJet, edm::Handle<edm::View<reco::Jet> >& recoJetCollection, edm::Handle<reco::JetTagCollection>& btagCollection, float dRMatch, bool debug ){
+
+  // loop over jets
+  for( edm::View<reco::Jet>::const_iterator jetToMatch = recoJetCollection->begin(); jetToMatch != recoJetCollection->end(); ++jetToMatch )
+    {
+      reco::Jet thisJet = *jetToMatch;
+      float dR = reco::deltaR( recJet->eta(),recJet->phi(), thisJet.eta(),thisJet.phi() );
+      if(dR > 0.1) continue;
+
+      size_t idx = (jetToMatch - recoJetCollection->begin());
+      edm::RefToBase<reco::Jet> jetRef = recoJetCollection->refAt(idx);
+
+      if(debug) std::cout << "btag discriminator value = " << (*btagCollection)[jetRef] << std::endl;
+      return (*btagCollection)[jetRef];
+  
+    }
+
+  if(debug){
+    std::cout << "ERROR  No btag match: " << std::endl;
+    
+    // loop over jets
+    for( edm::View<reco::Jet>::const_iterator jetToMatch = recoJetCollection->begin(); jetToMatch != recoJetCollection->end(); ++jetToMatch )
+      {
+	const reco::Jet thisJet = *jetToMatch;
+	std::cout << "\t Match attempt pt: " <<  thisJet.pt() << " vs " <<  recJet->pt()
+		  << " eta: " << thisJet.eta() << " vs " << recJet->eta()
+		  << "phi: "<< thisJet.phi() << " vs " << recJet->phi()
+		  << std::endl;
+	float dR = reco::deltaR( recJet->eta(),recJet->phi(), thisJet.eta(),thisJet.phi() );
+	std::cout << "dR " << dR << std::endl;
+      }
+  }    
+
+  return -99;
+}
+
+
+
+/*
+//____ Fill FC diphoton variables _____//
+void RecHitAnalyzer::fillFC ( const edm::Event& iEvent, const edm::EventSetup& iSetup ) {
+  edm::Handle<reco::PhotonCollection> photons;
+  iEvent.getByToken(photonCollectionT_, photons);
+  vFC_inputs_.clear();
+  int ptOrder[2] = {0, 1};
+  if ( vPho_[1].Pt() > vPho_[0].Pt() ) {
+      ptOrder[0] = 1;
+      ptOrder[1] = 0;
+  }
+  for ( int i = 0; i < 2; i++ ) {
+    vFC_inputs_.push_back( vPho_[ptOrder[i]].Pt()/m0_ );
+    vFC_inputs_.push_back( vPho_[ptOrder[i]].Eta() );
+  }
+  vFC_inputs_.push_back( TMath::Cos(vPho_[0].Phi()-vPho_[1].Phi()) );
+} // fillFC() 
+*/
+
+//define this as a plug-in
+
 //define this as a plug-in
 DEFINE_FWK_MODULE(ProducerTest);
