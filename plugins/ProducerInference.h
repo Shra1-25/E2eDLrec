cb67365b418f74ff5477839e77e3edfab2c0d257
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-28 01:49:55 +0530

    Update ProducerInference.h

--- plugins/ProducerInference.h
+++ plugins/ProducerInference.h
@@ -99,6 +99,8 @@ class ProducerInference : public edm::stream::EDProducer<> {
       std::vector<std::vector<float>> vEB_frame; //= std::vector<std::vector<float>> (vEB_frame_height,std::vector<float> (vEB_frame_width, 0.0));
       std::vector<float> vEB_flat_frame = std::vector<float> (vEB_frame_height*vEB_frame_width,0.0);
       std::vector<std::vector<float>> vEB_photon_frames;
+      std::vector<std::vector<float>> vECALstitched_frame;
+      std::vector<std::vector<float>> vTracksAtECALstitched_frame;
       std::vector<float> vclasses;
    
       unsigned int nPho;
