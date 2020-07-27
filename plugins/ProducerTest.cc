c71ecdc56a779a3013835cf56751567242d83e7c
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-28 03:17:42 +0530

    Update ProducerTest.cc

--- plugins/ProducerTest.cc
+++ plugins/ProducerTest.cc
@@ -170,6 +170,7 @@ ProducerTest::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
      std::unique_ptr<std::vector<int>> JetSeediphi_edm (new std::vector<int>(vJetSeed_iphi_));
      iEvent.put(std::move(JetSeedieta_edm),"JetSeedieta");
      iEvent.put(std::move(JetSeediphi_edm),"JetSeediphi");
+     vJetSeed_ieta_.clear(); vJetSeed_iphi_.clear();
    } else {
      passedSelection = runEvtSel( iEvent, iSetup );
      std::cout<<" >> Size of JetSeed vector (JetSeed_eta_size, JetSeed_phi_size) is: ("<<vJetSeed_ieta_.size()<<", "<<vJetSeed_iphi_.size()<<")"<<std::endl;
