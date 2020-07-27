ce860c89c79c7590ce564dd18fdea965b4ea640b
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-28 01:40:16 +0530

    Update FrameInference_cfi.py

--- python/FrameInference_cfi.py
+++ python/FrameInference_cfi.py
@@ -4,6 +4,10 @@ FrameInference = cms.EDProducer('ProducerInference'
     , reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
     , photonCollection = cms.InputTag('gedPhotons')
     , EBEnergy = cms.InputTag('ProducerFrames','EBenergy')
+    , ECALstitchedenergy = cms.InputTag('ProducerFrames','ECALstitchedenergy')
+    , TracksAtECALstitched = cms.InputTag('ProducerFrames', 'TracksAtECALstitched')
+    , JetSeedieta = cms.InputTag('ProducerFrames','JetSeedieta')
+    , JetSeediphi = cms.InputTag('ProducerFrames','JetSeediphi')
     #, mode = cms.string("JetLevel")
     # Jet level cfg
     #, nJets = cms.int32(-1)
