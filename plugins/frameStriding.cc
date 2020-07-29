920169319a38772528925b7e991477ad8c482961
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-07-30 02:52:40 +0530

    Update frameStriding.cc

--- plugins/frameStriding.cc
+++ plugins/frameStriding.cc
@@ -2,12 +2,12 @@
 
 std::vector<std::vector<float>> ProducerInference::frameStriding(std::vector<float>& vDetFrame, int rows, int columns, int rowstrides, int colstrides){
   std::vector<std::vector<float>> vStridedFrame ((rows*rowstrides), std::vector<float> (columns*colstrides,0));
-  for (int rowidx=0; rowidx<rows; rowidx+=rowstrides){
-    for (int colidx=0; colidx<columns; colidx+=rowstrides){
+  for (int rowidx=0; rowidx<rows; rowidx++){
+    for (int colidx=0; colidx<columns; colidx++){
       for (int kernelrow=0; kernelrow<rowstrides; kernelrow++){
         for (int kernelcol=0; kernelcol<colstrides; kernelcol++){
-          vStridedFrame[rowidx+kernelrow][colidx+kernelcol] = vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
-          //std::cout<<"("<<rowidx+kernelrow<<","<<colidx+kernelcol<<"): "<<vStridedFrame[rowidx+kernelrow][colidx+kernelcol]<<" "<<vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
+          vStridedFrame[rowstrides*rowidx+kernelrow][colstrides*colidx+kernelcol] = vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
+          //std::cout<<"("<<rowstrides*rowidx+kernelrow<<","<<colstrides*colidx+kernelcol<<"): "<<vStridedFrame[rowstrides*rowidx+kernelrow][colstrides*colidx+kernelcol]<<" "<<vDetFrame[rowidx*columns+colidx]/(rowstrides*colstrides);
         }
       }
     }
