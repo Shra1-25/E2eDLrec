ff2f93bf70a352832d210a40f59cf9c49a0035d9
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 01:50:05 +0530

    Update croppingFrames.h

--- plugins/croppingFrames.h
+++ plugins/croppingFrames.h
@@ -4,6 +4,7 @@
 /*#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/EGProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/QGProducer.h"*/
+#include <iostream>
 #include <vector>
 using namespace std;
 
