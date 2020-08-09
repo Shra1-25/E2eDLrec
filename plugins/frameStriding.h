318d95a89d6987d7a3e0bfbd2bf14ba23fec0142
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 01:10:36 +0530

    Update frameStriding.h

--- plugins/frameStriding.h
+++ plugins/frameStriding.h
@@ -5,7 +5,7 @@ using namespace std;
 
 std::vector<std::vector<float>> frameStriding(std::vector<float>&, int, int, int, int){
 std::vector<std::vector<float>> vStridedFrame ((rows*rowstrides), std::vector<float> (columns*colstrides,0));
-return vStridedFrame
+return vStridedFrame;
 }
 
 #endif
