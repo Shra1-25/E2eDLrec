32d5701344e2dab53e87c94820ac114814aa1c06
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 04:03:16 +0530

    Update EBRecHit_cfi.py

--- python/EBRecHit_cfi.py
+++ python/EBRecHit_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms 
 
-ProducerFrames = cms.EDProducer('ProducerTest'
+ProducerFrames = cms.EDProducer('DetImgProducer'
     , reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
     , photonCollection = cms.InputTag('gedPhotons')#or 'slimmedPhotons' for mini AOD root file
     , reducedHBHERecHitCollection = cms.InputTag('reducedHcalRecHits:hbhereco')
