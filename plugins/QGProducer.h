2030a02e762c1a39914a0d8952f0ff730ec3224d
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 03:37:25 +0530

    Update QGProducer.h

--- plugins/QGProducer.h
+++ plugins/QGProducer.h
@@ -59,11 +59,6 @@ using pat::PhotonRef;*/
 using reco::PhotonCollection;
 using reco::PhotonRef;
 
-static const int vEB_energy_height=170;
-static const int vEB_energy_width=360;
-static const int vEB_frame_height=32;
-static const int vEB_frame_width=32;
-
 class QGProducer : public edm::stream::EDProducer<> {
    public:
       
