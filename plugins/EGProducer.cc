d31a20fd174df87adc80d08edb9dd4b12a91dc6d
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 00:28:28 +0530

    Update EGProducer.cc

--- plugins/EGProducer.cc
+++ plugins/EGProducer.cc
@@ -1,5 +1,5 @@
-#include "ProdTutorial/ProducerTest/plugins/ProducerTest.h"
-#include "ProdTutorial/ProducerTest/plugins/ProducerInference.h"
+#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/QGProducer.h"
 #include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
 //#include <fstream>
 
