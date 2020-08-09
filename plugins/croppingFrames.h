9fcb738b885f4c325be68f5451f2bf8df717dcba
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 02:00:40 +0530

    Update croppingFrames.h

--- plugins/croppingFrames.h
+++ plugins/croppingFrames.h
@@ -8,8 +8,8 @@
 #include <vector>
 using namespace std;
 
-std::vector<std::vector<float>> croppingFrames(std::vector<float>& vdetector_image, int ieta_seed, int iphi_seed, int detImg_height, int detImg_width, int frame_height, int frame_width){
-std::vector<std::vector<float>> vframe = std::vector<std::vector<float>> (frame_height,std::vector<float> (frame_width, 0.0));
+std::vector<std::vector<float>> croppingFrames(std::vector<float>&, int, int, int, int, int, int){
+std::vector<std::vector<float>> vframe;
 return vframe;
 };
 #endif
