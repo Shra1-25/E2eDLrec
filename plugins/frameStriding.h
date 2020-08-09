01ff9c11ab1989ee900db306d3e62481d65dfeff
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 01:41:41 +0530

    Update frameStriding.h

--- plugins/frameStriding.h
+++ plugins/frameStriding.h
@@ -1,6 +1,6 @@
 #ifndef frameStriding_h
 #define frameStriding_h
-
+#include <vector>
 using namespace std;
 
 std::vector<std::vector<float>> frameStriding(std::vector<float>&, int, int, int, int){
