3de3e3d8d71d58b8156edd0ded427673a15af4e0
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 01:41:24 +0530

    Update croppingFrames.h

--- plugins/croppingFrames.h
+++ plugins/croppingFrames.h
@@ -4,7 +4,7 @@
 /*#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/EGProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/QGProducer.h"*/
-
+#include <vector>
 using namespace std;
 
 std::vector<std::vector<float>> croppingFrames(std::vector<float>&, int, int, int, int, int, int){
