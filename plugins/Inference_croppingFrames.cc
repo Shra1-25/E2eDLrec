025bfbf94f9cbacd861dff8c202fc3df8f505d80
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 01:18:32 +0530

    Update Inference_croppingFrames.cc

--- plugins/Inference_croppingFrames.cc
+++ plugins/Inference_croppingFrames.cc
@@ -2,7 +2,7 @@
 #include "ProdTutorial/ProducerTest/plugins/EGProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/QGProducer.h"*/
 #include "ProdTutorial/ProducerTest/plugins/croppingFrames.h"
-using namespace std;
+//using namespace std;
 
 std::vector<std::vector<float>> croppingFrames(std::vector<float>& vdetector_image, int ieta_seed, int iphi_seed, int detImg_height, int detImg_width, int frame_height, int frame_width){
   std::vector<std::vector<float>> vframe = std::vector<std::vector<float>> (frame_height,std::vector<float> (frame_width, 0.0));
