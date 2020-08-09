2ec7856764ed77f4516f4973fbe803bad6964962
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 04:06:55 +0530

    Update EGInference_cfi.py

--- python/EGInference_cfi.py
+++ python/EGInference_cfi.py
@@ -11,18 +11,3 @@ EGInference = cms.EDProducer('EGProducer'
     #, maxJetEta = cms.double(2.4)
     #, z0PVCut  = cms.double(0.1)
     )
-
-QGInference = cms.EDProducer('QGProducer'
-    , reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB')
-    , ECALstitchedenergy = cms.InputTag('ProducerFrames','ECALstitchedenergy')
-    , TracksAtECALstitched = cms.InputTag('ProducerFrames', 'TracksAtECALstitched')
-    , JetSeedieta = cms.InputTag('ProducerFrames','JetSeedieta')
-    , JetSeediphi = cms.InputTag('ProducerFrames','JetSeediphi')
-    , HBHEenergy = cms.InputTag('ProducerFrames','HBHEenergy')
-    #, mode = cms.string("JetLevel")
-    # Jet level cfg
-    #, nJets = cms.int32(-1)
-    #, minJetPt = cms.double(35.)
-    #, maxJetEta = cms.double(2.4)
-    #, z0PVCut  = cms.double(0.1)
-    )
