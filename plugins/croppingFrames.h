91525df1e2cf0e3d8b2dd9c62d2bde262b342fb5
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 01:20:01 +0530

    Create croppingFrames.h

---
+++ plugins/croppingFrames.h
@@ -0,0 +1,14 @@
+#ifndef frameStriding_h
+#define frameStriding_h
+
+/*#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/EGProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/QGProducer.h"*/
+
+using namespace std;
+
+std::vector<std::vector<float>> croppingFrames(std::vector<float>&, int, int, int, int, int, int){
+std::vector<std::vector<float>> vframe = std::vector<std::vector<float>> (frame_height,std::vector<float> (frame_width, 0.0));
+return vFrame;
+};
+#endif
