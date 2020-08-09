9c21f52a0877dbc4c67577ccb73b671e238b293e
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 01:20:57 +0530

    Update QGProducer.cc

--- plugins/QGProducer.cc
+++ plugins/QGProducer.cc
@@ -1,6 +1,7 @@
 #include "ProdTutorial/ProducerTest/plugins/QGProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/predict_tf.h"
+#include "ProdTutorial/ProducerTest/plugins/croppingFrames.h"
 #include "ProdTutorial/ProducerTest/plugins/frameStriding.h"
 #include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
 #include <fstream>
