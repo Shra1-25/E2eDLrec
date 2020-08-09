2df42c82ebf9955e20202ca274705261dd911ed6
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 01:17:14 +0530

    Update Inference_croppingFrames.cc

--- plugins/Inference_croppingFrames.cc
+++ plugins/Inference_croppingFrames.cc
@@ -1,7 +1,10 @@
-#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
-#include "ProdTutorial/ProducerTest/plugins/ProducerInference.h"
+/*#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/EGProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/QGProducer.h"*/
+#include "ProdTutorial/ProducerTest/plugins/croppingFrames.h"
+using namespace std;
 
-std::vector<std::vector<float>> ProducerInference::croppingFrames(std::vector<float>& vdetector_image, int ieta_seed, int iphi_seed, int detImg_height, int detImg_width, int frame_height, int frame_width){
+std::vector<std::vector<float>> croppingFrames(std::vector<float>& vdetector_image, int ieta_seed, int iphi_seed, int detImg_height, int detImg_width, int frame_height, int frame_width){
   std::vector<std::vector<float>> vframe = std::vector<std::vector<float>> (frame_height,std::vector<float> (frame_width, 0.0));
   int start_x=0;
   int end_x=0;
