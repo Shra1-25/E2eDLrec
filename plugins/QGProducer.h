7957a6a3b014e93bc33a526db31da41b8e85ddf6
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-10 16:29:08 +0530

    Update QGProducer.h

--- plugins/QGProducer.h
+++ plugins/QGProducer.h
@@ -155,10 +155,10 @@ class QGProducer : public edm::stream::EDProducer<> {
       double maxJetEta_;
       double z0PVCut_;
    
-      const reco::PFCandidate* getPFCand(edm::Handle<PFCollection> pfCands, float eta, float phi, float& minDr, bool debug = false);
+      /*const reco::PFCandidate* getPFCand(edm::Handle<PFCollection> pfCands, float eta, float phi, float& minDr, bool debug = false);
       const reco::Track* getTrackCand(edm::Handle<reco::TrackCollection> trackCands, float eta, float phi, float& minDr, bool debug = false);
       int   getTruthLabel(const reco::PFJetRef& recJet, edm::Handle<reco::GenParticleCollection> genParticles, float dRMatch = 0.4, bool debug = false);
-      float getBTaggingValue(const reco::PFJetRef& recJet, edm::Handle<edm::View<reco::Jet> >& recoJetCollection, edm::Handle<reco::JetTagCollection>& btagCollection, float dRMatch = 0.1, bool debug= false );
+      float getBTaggingValue(const reco::PFJetRef& recJet, edm::Handle<edm::View<reco::Jet> >& recoJetCollection, edm::Handle<reco::JetTagCollection>& btagCollection, float dRMatch = 0.1, bool debug= false );*/
       /*TTree* RHTree;
       unsigned int nPho;
       
