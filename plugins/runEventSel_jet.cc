b03bcf048624968b4515569165bc51ceb6813f19
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-11 12:05:18 +0530

    Update runEventSel_jet.cc

--- plugins/runEventSel_jet.cc
+++ plugins/runEventSel_jet.cc
@@ -49,8 +49,63 @@ bool QGProducer::runEvtSel_jet ( const edm::Event& iEvent, const edm::EventSetup
     	seedE = ( iRHit == HBHERecHitsH_->end() ) ? 0. : iRHit->energy() ;
     	HcalDetId seedId = hId;
     	if ( debug ) std::cout << " >> hId.ieta:" << hId.ieta() << " hId.iphi:" << hId.iphi() << " E:" << seedE << std::endl;
-   }
 	
+	// Look for the most energetic HBHE tower deposit within a search window
+	for ( int ieta = 0; ieta < search_window; ieta++ ) {
+
+      		ieta_ = hId.ieta() - (search_window/2)+ieta;
+
+      		if ( std::abs(ieta_) > HBHE_IETA_MAX_HE-1 ) continue;
+      		if ( std::abs(ieta_) < HBHE_IETA_MIN_HB ) continue;
+
+      		HcalSubdetector subdet_ = std::abs(ieta_) > HBHE_IETA_MAX_HB ? HcalEndcap : HcalBarrel;
+
+      		for ( int iphi = 0; iphi < search_window; iphi++ ) {
+
+        		iphi_ = hId.iphi() - (search_window/2)+iphi;
+
+        		// iphi should wrap around
+        		if ( iphi_ > HBHE_IPHI_MAX ) {
+          		iphi_ = iphi_-HBHE_IPHI_MAX;
+        		} else if ( iphi_ < HBHE_IPHI_MIN ) {
+          		iphi_ = HBHE_IPHI_MAX-abs(iphi_); 
+        		}
+        		// Skip non-existent and lower energy towers 
+        		HcalDetId hId_( subdet_, ieta_, iphi_, 1 );
+        		HBHERecHitCollection::const_iterator iRHit( HBHERecHitsH_->find(hId_) );
+        		if ( iRHit == HBHERecHitsH_->end() ) continue;
+        		if ( iRHit->energy() <= seedE ) continue;
+        		if ( debug ) std::cout << " !! hId.ieta:" << hId_.ieta() << " hId.iphi:" << hId_.iphi() << " E:" << iRHit->energy() << std::endl;
+
+        		seedE = iRHit->energy();
+        		seedId = hId_;
+
+      		} // iphi 
+    	} // ieta
+	
+    	// NOTE: HBHE iphi = 1 does not correspond to EB iphi = 1!
+    	// => Need to shift by 2 HBHE towers: HBHE::iphi: [1,...,71,72]->[3,4,...,71,72,1,2]
+    	iphi_  = seedId.iphi() + 2; // shift
+    	iphi_  = iphi_ > HBHE_IPHI_MAX ? iphi_-HBHE_IPHI_MAX : iphi_; // wrap-around
+    	iphi_  = iphi_ - 1; // make histogram-friendly
+    	ietaAbs_  = seedId.ietaAbs() == HBHE_IETA_MAX_HE ? HBHE_IETA_MAX_HE-1 : seedId.ietaAbs(); // last HBHE ieta embedded
+    	ieta_  = seedId.zside() > 0 ? ietaAbs_-1 : -ietaAbs_;
+    	ieta_  = ieta_+HBHE_IETA_MAX_HE-1;
+
+    	// If the seed is too close to the edge of HE, discard event
+    	// Required to keep the seed at the image center
+    	if ( HBHE_IETA_MAX_HE-1 - ietaAbs_ < image_padding ) { 
+      	if ( debug ) std::cout << " Fail HE edge cut " << std::endl;
+      	vFailedJetIdx_.push_back(thisJetIdx);
+      	continue;
+    	}
+    	// Save position of most energetic HBHE tower
+    	// in EB-aligned coordinates
+    	if ( debug ) std::cout << " !! ieta_:" << ieta_ << " iphi_:" << iphi_ << " ietaAbs_:" << ietaAbs_ << " E:" << seedE << std::endl;
+    	vJetSeed_iphi_.push_back( iphi_ );
+    	vJetSeed_ieta_.push_back( ieta_ );
+    	nJet++;
+   } // good jets	
 	
 	
 // Remove jets that failed the Seed cuts 
