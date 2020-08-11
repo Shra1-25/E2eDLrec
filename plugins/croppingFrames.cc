910db730899cf53652ea046dfa9b748f0bd19648
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-11 21:40:33 +0530

    Update croppingFrames.cc

--- plugins/croppingFrames.cc
+++ plugins/croppingFrames.cc
@@ -1,8 +1,8 @@
 /*#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/EGProducer.h"
 #include "ProdTutorial/ProducerTest/plugins/QGProducer.h"*/
-#include "ProdTutorial/ProducerTest/plugins/EGProducer.h"
-#include "ProdTutorial/ProducerTest/plugins/QGProducer.h"
+//#include "ProdTutorial/ProducerTest/plugins/EGProducer.h"
+//#include "ProdTutorial/ProducerTest/plugins/QGProducer.h"
 //using namespace std;
 
 std::vector<std::vector<float>> croppingFrames(std::vector<float>& vdetector_image, int ieta_seed, int iphi_seed, int detImg_height, int detImg_width, int frame_height, int frame_width){
