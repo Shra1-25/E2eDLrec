30e3db11921129bcf4b9d2bb1652aef00c9e6cc4
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-22 21:04:55 +0530

    Update DetImgProducer.cc

--- plugins/DetImgProducer.cc
+++ plugins/DetImgProducer.cc
@@ -109,8 +109,37 @@ DetImgProducer::DetImgProducer(const edm::ParameterSet& iConfig)
     branchesEvtSel( RHTree, fs );
   }*/
 
+  ///////////adjustable granularity stuff
+
+  granularityMultiPhi[0]  = iConfig.getParameter<int>("granularityMultiPhi");
+  granularityMultiEta[0]  = iConfig.getParameter<int>("granularityMultiEta");
+
+  granularityMultiPhi[1] = 3;
+  granularityMultiEta[1] = 3;
+
+  for (unsigned int proj=0; proj<Nadjproj; proj++)
+  {
+  
+    int totalMultiEta = granularityMultiEta[proj] * granularityMultiECAL;
+
+    for (int i=0; i<eta_nbins_HBHE; i++)
+    {
+      double step=(eta_bins_HBHE[i+1]-eta_bins_HBHE[i])/totalMultiEta;
+      for (int j=0; j<totalMultiEta; j++)
+      {
+        adjEtaBins[proj].push_back(eta_bins_HBHE[i]+step*j);
+      }
+    }
+    adjEtaBins[proj].push_back(eta_bins_HBHE[eta_nbins_HBHE]);
+
+    totalEtaBins[proj] = totalMultiEta*(eta_nbins_HBHE);
+    totalPhiBins[proj] = granularityMultiPhi[proj] * granularityMultiECAL*HBHE_IPHI_NUM;
+
+  }
+	
  //usesResource("TFileService");
  edm::Service<TFileService> fs;
+ h_sel = fs->make<TH1F>("h_sel", "isSelected;isSelected;Events", 2, 0., 2.);
  RHTree = fs->make<TTree>("RHTree", "RecHit tree");
  //RHTree->Branch("SC_iphi", &vIphi_Emax_);
  //RHTree->Branch("SC_ieta", &vIeta_Emax_);
