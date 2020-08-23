7862b80bdcbe3dc35a82f06d9c835a2b9e287ec1
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-24 00:35:31 +0530

    Create TopProducer.cc

---
+++ plugins/TopProducer.cc
@@ -0,0 +1,7 @@
+#include "ProdTutorial/ProducerTest/plugins/TopProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/predict_tf.h"
+#include "ProdTutorial/ProducerTest/plugins/croppingFrames.h"
+#include "ProdTutorial/ProducerTest/plugins/frameStriding.h"
+#include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
+#include <fstream>
