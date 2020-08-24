345a785b8de649dedfbc1dcbfb5455229f6a134f
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-24 23:34:41 +0530

    Update TopProducer.cc

--- plugins/TopProducer.cc
+++ plugins/TopProducer.cc
@@ -195,10 +195,11 @@ TopProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
      vECALstitchedClass.push_back(-1);
      vTracksAtECALstitchedPtClass.push_back(-1);
      vTracksAtECALadjPtClass.push_back(-1);
+    
+     std::cout<<" >> TopInference Prediction of Stitched ECAL: "<<vECALstitchedClass[idx]<<std::endl;
+     std::cout<<" >> TopInference Prediction of Tracks at Stitched ECAL: "<<vTracksAtECALstitchedPtClass[idx]<<std::endl;
+     std::cout<<" >> TopInference Prediction of Tracks at ECAL adjustable: "<<vTracksAtECALadjPtClass[idx]<<std::endl;
     }
-    std::cout<<" >> TopInference Prediction of Stitched ECAL: "<<vECALstitchedClass[idx]<<std::endl;
-    std::cout<<" >> TopInference Prediction of Tracks at Stitched ECAL: "<<vTracksAtECALstitchedPtClass[idx]<<std::endl;
-    std::cout<<" >> TopInference Prediction of Tracks at ECAL adjustable: "<<vTracksAtECALadjPtClass[idx]<<std::endl;
    }
    std::cout<<std::endl; //Stitched ECAL and their track frames created.
    	
@@ -238,8 +239,9 @@ TopProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
    }*/
    vHBHEenergyClass.push_back(predict_tf(vHBHEenergy_frame, "qg_model.pb", "inputs", "softmax_1/Sigmoid"));
    }
-   else {vHBHEenergyClass.push_back(-1);}
-   std::cout<<" >> TopInference Prediction of HBHE energy: "<<vHBHEenergyClass[idx]<<std::endl;
+   else {vHBHEenergyClass.push_back(-1);
+     std::cout<<" >> TopInference Prediction of HBHE energy: "<<vHBHEenergyClass[idx]<<std::endl;
+    }
    }
    std::unique_ptr<std::vector<float>> vECALstitchedClass_edm (new std::vector<float>(vECALstitchedClass));
    iEvent.put(std::move(vECALstitchedClass_edm),"TopQECALstitchedClass");
