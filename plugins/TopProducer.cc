caf345a22c2199f3fa2e2f0d72d0ab9f50d1807e
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-24 18:10:02 +0530

    Update TopProducer.cc

--- plugins/TopProducer.cc
+++ plugins/TopProducer.cc
@@ -29,8 +29,7 @@ TopProducer::TopProducer(const edm::ParameterSet& iConfig)
  recoJetsT_              = consumes<edm::View<reco::Jet> >(iConfig.getParameter<edm::InputTag>("recoJetsForBTagging"));
  jetTagCollectionT_      = consumes<reco::JetTagCollection>(iConfig.getParameter<edm::InputTag>("jetTagCollection"));
  ipTagInfoCollectionT_   = consumes<std::vector<reco::CandIPTagInfo> > (iConfig.getParameter<edm::InputTag>("ipTagInfoCollection"));
- std::cout<<"Reading data collection done "<<nTotal<<std::endl;
- 
+  
  mode_      = iConfig.getParameter<std::string>("mode");
  minJetPt_  = iConfig.getParameter<double>("minJetPt");
  maxJetEta_ = iConfig.getParameter<double>("maxJetEta");
