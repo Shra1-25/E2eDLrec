ad7ae5846c64b4c709c4a6f457bfe663b1b0796d
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-27 12:32:27 +0530

    Update getPhoton.cc

--- plugins/getPhoton.cc
+++ plugins/getPhoton.cc
@@ -167,10 +167,11 @@ void EGProducer::get_photons ( const edm::Event& iEvent, const edm::EventSetup& 
   vpredictions.push_back(0);
   vpredictions[0] = predict_tf(vEB_frame, "e_vs_ph_model.pb","inputs","softmax_1/Sigmoid");
   for (int frame_x=0;frame_x<int(vEB_frame.size());frame_x++){
-    for (int frame_y=0;frame_y<int(vEB_frame[nPassed].size());frame_y++){
-      vEB_flat_frame[frame_x*vEB_frame[nPassed].size()+frame_y]=vEB_frame[frame_x][frame_y];
+    for (int frame_y=0;frame_y<int(vEB_frame[0].size());frame_y++){
+      vEB_flat_frame[frame_x*vEB_frame[0].size()+frame_y]=vEB_frame[frame_x][frame_y];
     }
   }
+  std::cout<<" >> Size of flat frame: "<<vEB_flat_frame.size()<<std::endl;
   photon_collection.putFrameCollection(vEB_flat_frame);
   photon_collection.putPredCollection(vpredictions);
   vpredictions.clear();
