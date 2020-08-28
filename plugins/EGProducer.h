4134e7be4f0d0b2336b3dee727946c90ab447a53
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-28 08:24:29 +0530

    Update EGProducer.h

--- plugins/EGProducer.h
+++ plugins/EGProducer.h
@@ -8,7 +8,8 @@
 #include <vector>
 
 // user include files
-#include "ProdTutorial/E2eDlRecClasses/interface/framePredCollection.h"
+//#include "ProdTutorial/E2eDlRecClasses/interface/framePredCollection.h"
+#include "ProdTutorial/ProducerTest/plugins/framePredCollection"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 //#include "FWCore/Framework/interface/one/EDAnalyzer.h"
