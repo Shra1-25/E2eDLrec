6ef7d93b705a1c72b7a5b25c2ab2b3f928cd8547
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-31 02:12:15 +0530

    Update TopProducer_runEventSel_jet.cc

--- plugins/TopProducer_runEventSel_jet.cc
+++ plugins/TopProducer_runEventSel_jet.cc
@@ -1,5 +1,7 @@
-#include "ProdTutorial/ProducerTest/plugins/TopProducer.h"
-#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
+/*#include "ProdTutorial/ProducerTest/plugins/TopProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"*/
+#include "E2eDLrec/plugins/TopProducer.h"
+#include "E2eDLrec/plugins/DetImgProducer.h"
 
 using std::vector;
 
