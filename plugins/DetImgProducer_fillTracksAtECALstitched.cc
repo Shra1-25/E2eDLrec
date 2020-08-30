2789d1b7a2c845524f34c8498f6814a1aa635720
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-31 02:34:36 +0530

    Update DetImgProducer_fillTracksAtECALstitched.cc

--- plugins/DetImgProducer_fillTracksAtECALstitched.cc
+++ plugins/DetImgProducer_fillTracksAtECALstitched.cc
@@ -1,5 +1,5 @@
 //#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
-#include "E2eDLrec/plugins/DetImgProducer.h"
+#include "E2eDL/E2eDLrec/plugins/DetImgProducer.h"
 
 // Fill Tracks into stitched EEm_EB_EEp image //////////////////////
 // Store all Track positions into a stitched EEm_EB_EEp image 
