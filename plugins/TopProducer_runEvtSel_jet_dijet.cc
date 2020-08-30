c620aa85d253991b7e67c7379ff217d6158db0bc
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-31 02:39:57 +0530

    Update TopProducer_runEvtSel_jet_dijet.cc

--- plugins/TopProducer_runEvtSel_jet_dijet.cc
+++ plugins/TopProducer_runEvtSel_jet_dijet.cc
@@ -1,6 +1,9 @@
 /*#include "ProdTutorial/ProducerTest/plugins/TopProducer.h"
 #include "DataFormats/BTauReco/interface/JetTag.h"
-#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"*/
+/*#include "E2eDL/E2eDLrec/plugins/TopProducer.h"
+#include "DataFormats/BTauReco/interface/JetTag.h"
+#include "E2eDL/E2eDLrec/plugins/DetImgProducer.h
 
 using std::vector;
 using std::cout;
