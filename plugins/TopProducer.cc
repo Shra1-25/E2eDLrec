1a0281ef9523ac647a7bb63a7987b892a39c6520
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-30 22:01:08 +0530

    Update TopProducer.cc

--- plugins/TopProducer.cc
+++ plugins/TopProducer.cc
@@ -112,7 +112,7 @@ TopProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
    	if (temp_frame.size()==0) std::cout<<" >> Empty photon frame collection for all the seeds."<<std::endl;
     	else {
 		std::cout<<" >> Size of temp_frame: ("<<temp_frame.size()<<", "<<temp_frame[0].size()<<std::endl;
-    		std::vector<float> temp_pred=predict_tf(temp_frame,"ResNet.pb","inputs","softmax_1/Sigmoid");
+    		std::vector<float> temp_pred=predict_tf(temp_frame,"ResNet.pb","inputs","outputs");
 		std::cout<<" >> Class Object model predictions of seed "<<frameidx<<"/"<<qgJetCollection_itr.size()<<" are: [";
     		for (int idx=0;idx<int(temp_pred.size());idx++){
       			std::cout<<temp_pred[idx]<<", ";
