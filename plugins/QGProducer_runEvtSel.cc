96483692d3e0c4046246f2ab1be447d54996dc55
Shra1-25 <42185680+Shra1-25@users.noreply.github.com>
2020-08-31 02:03:39 +0530

    Update QGProducer_runEvtSel.cc

--- plugins/QGProducer_runEvtSel.cc
+++ plugins/QGProducer_runEvtSel.cc
@@ -1,5 +1,7 @@
-#include "ProdTutorial/ProducerTest/plugins/QGProducer.h"
-#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"
+/*#include "ProdTutorial/ProducerTest/plugins/QGProducer.h"
+#include "ProdTutorial/ProducerTest/plugins/DetImgProducer.h"*/
+#include "E2eDLrec/plugins/QGProducer.h"
+#include "E2eDLrec/plugins/DetImgProducer.h"
 // Run event selection ////////////////////////////////
 
 TH1F *h_m0;
